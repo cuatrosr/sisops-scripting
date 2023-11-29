3d0983a8ac328d7f1b6f43f19859ca1c0d050c00
KerenLopez <kerenlopezcordoba@outlook.es>
2023-11-29 01:32:27 -0500

    feat: disable user

---
+++ modules/users/disableUser.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Solicitar el nombre de usuario
+read -p "Ingrese el nombre de usuario: " username
+
+# Buscar y obtener la info del usuario de la BD
+user_info=$(grep -i "| $username |" usersBD.txt)
+
+# Verificar si el usuario existe
+if [ -n "$user_info" ]; then
+    # Obtener el UID del usuario
+    uid=$(echo "$user_info" | cut -d"|" -f1 | tr -d ' ')
+
+    # Eliminar el usuario
+    delete_output=$(sudo userdel -r "$username" 2>&1)
+
+    # Cambiar el estado del usuario a deshabilitado
+    user_info_modified=$(echo "$user_info" | awk -F"|" '{$4=" deshabilitado"} 1' OFS="|")
+    sed -i "s~$user_info~$user_info_modified~" usersBD.txt
+    echo "El usuario $username fue deshabilitado correctamente."
+else
+    echo "El usuario $username no fue encontrado en la BD, inténtalo de nuevo."
+fi
