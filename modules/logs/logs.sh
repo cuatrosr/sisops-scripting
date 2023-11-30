47246b01d8e7204cfbf072d5c980a85d40c4f19c
Laura-Martinez182 <lauramartinezo182@gmail.com>
2023-11-30 00:44:36 -0500

    feat: logs menu added

--- modules/logs/logs.sh
+++ modules/logs/logs.sh
@@ -1,3 +1,37 @@
 #!/bin/bash
 
-echo "Logs!"
+while true; do
+    echo "Gestion de Logs"
+    echo "1. Ver los inicios de sesion fallidos"
+    echo "2. Ver los cambios de contraseña realizados"
+    echo "3. Numero de veces que el usuario inicio sesion"
+    echo "4. Numero de veces que el usuario cerro sesion"
+    
+    echo "0. Regresar al menú principal"
+
+    read -r -p "Seleccione una opción: " opcion
+    echo
+
+    case $opcion in
+    1)
+        sh modules/logs/authFailure.sh
+        ;;
+    2)
+        sh modules/logs/passwordChanged.sh
+        ;;
+    3)
+        sh modules/logs/openedSessions.sh
+        ;;
+        
+    4)
+        sh modules/logs/closedSessions.sh
+        ;;
+    0)
+    	echo "Volviendo al menú principal..."
+        break
+	;;
+    *)
+        echo "Opción no válida"
+        ;;
+    esac
+done
